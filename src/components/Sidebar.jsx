f5a7b6913358ff66bde1648bb3357cb3e180e228
IRFI4 <vladisglyadelkin@gmail.com>
2024-07-25 23:55:43 +0300

    created TopPlay component, changed Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
           <item.icon className="w-6 h-6 mr-2" />
           {item.name}
         </NavLink>
-      ))};
+      ))}
     </div>
   );
 
@@ -37,7 +37,7 @@ const Sidebar = () => {
           <RiCloseLine className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(false)} />
         ) : (
           <HiOutlineMenu className="w-6 h-6 text-white mr-2" onClick={() => setMobileMenuOpen(true)} />
-        )};
+        )}
       </div>
 
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
